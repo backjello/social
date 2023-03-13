4c5ede13c5f46d33e323253c36c839ac597b8b0f
backjello <jackbello97@gmail.com>
2023-03-13 12:24:53 +0100

    commit

--- src/app/components/bottonecommenti/bottonecommenti.component.ts
+++ src/app/components/bottonecommenti/bottonecommenti.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Commento } from 'src/app/interface/commento';
 import { Post } from 'src/app/interface/post';
 import { ApiService } from 'src/app/services/api.service';
+import { PopupcommentiComponent } from '../popupcommenti/popupcommenti.component';
 
 @Component({
   selector: 'app-bottonecommenti',
@@ -11,13 +14,25 @@ export class BottonecommentiComponent implements OnInit {
 
   @Input() post! : Post
   nCommenti! : number;
+  comments:Commento[] = []
 
   // nel costruttore post è undefined
-  constructor(private api:ApiService) { }
+  constructor(private api:ApiService, 
+    private dialog:MatDialog
+  ) { }
+
+  mostraCommenti(){
+    this.dialog.open(PopupcommentiComponent,{
+      data:this.post // passiamo il post alla modal 
+    })
+  }
 
   ngOnInit(): void {
     this.api.getComments(this.post).subscribe((res)=>{
+      //res = {total:3, comments:[...]}
       this.nCommenti = res.total
+      this.comments = res.comments
+      this.post.comments = this.comments
     })
   }
 
